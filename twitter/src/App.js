958f311f3ec75edf037fda5f8337ecc55ecf9acd
ichillous <isiah.chillous@gmail.com>
2020-09-11 10:58:50 -0400

    installed react router dom

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Home from "./components/Home";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 import About from "./components/About"
-import { Router } from "react-router-dom";
 
 function App() {
   return (
