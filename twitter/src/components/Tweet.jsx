c5d0c9e6893a36581228b741c8368dfd5a8e90bc
ichillous <isiah.chillous@gmail.com>
2020-09-11 11:04:13 -0400

    debugging import error

--- twitter/src/components/Tweet.jsx
+++ twitter/src/components/Tweet.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {  Link } from "react-router-dom";
-import UpdateTweet from "./UpdateTweet";
+
 
 const Tweet = (props) => {
   const name = props.tweet.fields.name;
   const text = props.tweet.fields.text;
-  
+
   return (
     <div id="tweet">
       <h5>{name}</h5>
